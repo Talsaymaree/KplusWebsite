39a8e38c5c9158d78a3378a98fd5d87bcd23f08a
Talal <Talal.alsaymaree@gmail.com>
2020-05-06 22:25:36 -0400

    coming soon is done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,64 @@
-import React from "react";
-import { Link } from "gatsby";
+import React, { useState, useEffect } from "react";
+import { Container, H1, Caret } from "./styled";
 
-import Layout from "../components/Layout/Layout";
-import Image from "../components/image";
-import SEO from "../components/seo";
+const IndexPage = () => {
+  const [displayedText, updateDisplayText] = useState("");
+  const [currentPosition, incrementCurrentPosition] = useState(0);
+  const [tick, incrementTick] = useState(0);
+  const [currentTxtPos, incrementCurrentTxtPos] = useState(0);
+  const [isDecrement, reverseDecrement] = useState(false);
+  var dataText = ["Coming Soon...", "...قريبا", " demnächst"];
 
-const IndexPage = () => (
-  <div>
-    <p>Coming soon</p>
-  </div>
-);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      incrementTick(prev => prev + 1);
+      // console.log(
+      //   "just testing this," + currentPosition + " , " + displayedText
+      // );
+    }, 150);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    typeWriter();
+  }, [tick]);
+
+  const typeWriter = () => {
+    if (currentPosition < dataText[currentTxtPos].length && !isDecrement) {
+      incrementCurrentPosition(prev => prev + 1);
+      // console.log("incrementi/ng");
+      updateDisplayText(
+        dataText[currentTxtPos].substring(0, currentPosition + 1)
+      );
+    } else {
+      if (currentPosition > 0) {
+        incrementCurrentPosition(prev => prev - 1);
+        // console.log("decrementing");
+        reverseDecrement(true);
+        updateDisplayText(
+          dataText[currentTxtPos].substring(0, currentPosition - 1)
+        );
+      } else {
+        reverseDecrement(false);
+        if (currentTxtPos < dataText.length - 1) {
+          // console.log("it's changing for new word");
+          incrementCurrentTxtPos(prev => prev + 1);
+        } else {
+          // console.log("it's going back to first word");
+          incrementCurrentTxtPos(0);
+        }
+      }
+    }
+  };
+
+  return (
+    <Container>
+      <H1 variant="h2">
+        {displayedText}
+        <Caret />
+      </H1>
+    </Container>
+  );
+};
 
 export default IndexPage;
