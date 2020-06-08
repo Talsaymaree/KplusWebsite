4554abb51e7fad38469e234a2406680e0d07b5b5
fabrice <fnduister@gmail.com>
2020-06-08 19:59:45 -0400

    added css for react-slick

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,17 @@ const IndexPage = () => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>main section</title>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
         <link rel="canonical" href="http://mysite.com/example" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Helmet>
@@ -25,7 +36,7 @@ const IndexPage = () => {
         <AlbumShowcases />
         <SectionHeader title="Latest Posts" subtitle="Checkout What's new at our blog" />
         <PaperCard>
-        <SectionHeader title="Most Popular This Week" subtitle="Checkout What's new at our blog" />
+          <SectionHeader title="Most Popular This Week" subtitle="Checkout What's new at our blog" />
         </PaperCard>
       </MainContent>
       <Footer />
